f1530db22b4c19debacf613683af6b88e029a885
Tejash Patel <pateltejash429@gmail.com>
2023-02-05 23:49:47 +0530

    Solved Alert Error

--- script.js
+++ script.js
@@ -14,41 +14,40 @@ if (todos.length !== 0) {
 //Adding Todo to LocalStorage
 const addTodo = todo => {
   todos.push({ todo });
-
   localStorage.setItem('todos', JSON.stringify(todos));
-
   return { todo };
 };
 
 //Creating a New Todo
 const createTodoElement = ({ todo }) => {
-  if (todo.value === '') {
-    alert('Enter a Todo');
-  } else {
-    const todoDiv = document.createElement('div');
-    todoDiv.setAttribute('class', 'todo');
+  const todoDiv = document.createElement('div');
+  todoDiv.setAttribute('class', 'todo');
 
-    const newTodo = document.createElement('p');
-    newTodo.setAttribute('class', 'task');
-    newTodo.innerHTML = todo;
+  const newTodo = document.createElement('p');
+  newTodo.setAttribute('class', 'task');
+  newTodo.innerHTML = todo;
 
-    const checkTodo = document.createElement('input');
-    checkTodo.setAttribute('type', 'checkbox');
-    checkTodo.setAttribute('class', 'checkBox');
+  const checkTodo = document.createElement('input');
+  checkTodo.setAttribute('type', 'checkbox');
+  checkTodo.setAttribute('class', 'checkBox');
 
-    todoDiv.appendChild(checkTodo);
-    todoDiv.appendChild(newTodo);
-    todoContainer.append(todoDiv);
-  }
+  todoDiv.appendChild(checkTodo);
+  todoDiv.appendChild(newTodo);
+  todoContainer.append(todoDiv);
 };
 
 todos.forEach(createTodoElement);
 
 addBtn.addEventListener('click', function () {
-  const newTodo = addTodo(inputTodo.value);
-  createTodoElement(newTodo);
-  inputTodo.value = '';
-  if (todos.length !== 0) {
-    document.querySelector('.anime').hidden = true;
+  if (inputTodo.value === '') {
+    alert('Enter a Todo');
+  } else {
+    console.log('done');
+    const newTodo = addTodo(inputTodo.value);
+    createTodoElement(newTodo);
+    inputTodo.value = '';
+    if (todos.length !== 0) {
+      document.querySelector('.anime').hidden = true;
+    }
   }
 });
